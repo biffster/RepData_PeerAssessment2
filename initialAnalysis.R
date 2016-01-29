2047df53f86fc63d1f7b89868c0b78327aacb6ba
Michael Fierro <mifierro@cisco.com>
2016-01-28 22:09:19 -0700

    Data returned and charg made for Health

--- initialAnalysis.R
+++ initialAnalysis.R
@@ -1,8 +1,12 @@
 library(plyr)
 library(dplyr)
+library(ggplot2)
+
 weatherData <- read.csv("repdata-data-StormData.csv.bz2", header = TRUE, na.strings = "NA")
 healthWeatherData <- data.frame(EVTYPE=factor(), FATALITIES=numeric(), INJURIES=numeric())
 healthWeatherData <- weatherData[,c(8,23,24)]
 healthComputed <- mutate(healthWeatherData, HealthTotal=FATALITIES+INJURIES)
 healthSums <- ddply(healthComputed, .(EVTYPE), summarize, HealthTotal=sum(HealthTotal))
-
+topHealth <- head(healthSums[order(HealthTotal, na.last = TRUE, decreasing = TRUE),])
+ggplot(topHealth, aes(x = factor(EVTYPE), y = HealthTotal)) + geom_bar(stat = "identity")
+ggplot(topHealth, aes(x = factor(EVTYPE), y = HealthTotal)) + geom_bar(stat = "identity", fill = "#FF9999")
