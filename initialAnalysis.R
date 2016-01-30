0d262c722d4b628c2983905c7f7c7f402419428d
Michael Fierro <mifierro@cisco.com>
2016-01-30 15:45:03 -0700

    First round through the RMarkdown file

--- initialAnalysis.R
+++ initialAnalysis.R
@@ -8,7 +8,6 @@ healthWeatherData <- weatherData[,c(8,23,24)]
 healthComputed <- mutate(healthWeatherData, HealthTotal=FATALITIES+INJURIES)
 healthSums <- ddply(healthComputed, .(EVTYPE), summarize, HealthTotal=sum(HealthTotal))
 topHealth <- head(healthSums[order(healthSums$HealthTotal, na.last = TRUE, decreasing = TRUE),])
-ggplot(topHealth, aes(x = factor(EVTYPE), y = HealthTotal)) + geom_bar(stat = "identity")
 ggplot(topHealth, aes(x = factor(EVTYPE), y = HealthTotal)) + geom_bar(stat = "identity", fill = "#FF9999")
 
 econWeatherData <- data.frame(EVTYPE=factor(), PROPDMG=numeric(), CROPDMG=numeric())
@@ -16,5 +15,4 @@ econWeatherData <- weatherData[,c(8,25,27)]
 econComputed <- mutate(econWeatherData, econTotal=PROPDMG+CROPDMG)
 econSums <- ddply(econComputed, .(EVTYPE), summarize, econTotal=sum(econTotal))
 topEcon <- head(econSums[order(econSums$econTotal, na.last = TRUE, decreasing = TRUE),])
-ggplot(topEcon, aes(x = factor(EVTYPE), y = econTotal)) + geom_bar(stat = "identity")
 ggplot(topEcon, aes(x = factor(EVTYPE), y = econTotal)) + geom_bar(stat = "identity", fill = "#FF9999")
