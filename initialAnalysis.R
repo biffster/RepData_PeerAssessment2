4a4e4516198315bf19ba4c5e2fedf302a296f818
Michael Fierro <mifierro@cisco.com>
2016-01-30 11:17:19 -0700

    Syncing with laptop

--- initialAnalysis.R
+++ initialAnalysis.R
@@ -1,8 +1,8 @@
 library(plyr)
 library(dplyr)
 library(ggplot2)
-
 weatherData <- read.csv("repdata-data-StormData.csv.bz2", header = TRUE, na.strings = "NA")
+
 healthWeatherData <- data.frame(EVTYPE=factor(), FATALITIES=numeric(), INJURIES=numeric())
 healthWeatherData <- weatherData[,c(8,23,24)]
 healthComputed <- mutate(healthWeatherData, HealthTotal=FATALITIES+INJURIES)
@@ -10,3 +10,11 @@ healthSums <- ddply(healthComputed, .(EVTYPE), summarize, HealthTotal=sum(Health
 topHealth <- head(healthSums[order(HealthTotal, na.last = TRUE, decreasing = TRUE),])
 ggplot(topHealth, aes(x = factor(EVTYPE), y = HealthTotal)) + geom_bar(stat = "identity")
 ggplot(topHealth, aes(x = factor(EVTYPE), y = HealthTotal)) + geom_bar(stat = "identity", fill = "#FF9999")
+
+econWeatherData <- data.frame(EVTYPE=factor(), PROPDMG=numeric(), CROPDMG=numeric())
+econWeatherData <- weatherData[,c(8,25,27)]
+econComputed <- mutate(econWeatherData, econTotal=PROPDMG+CROPDMG)
+econSums <- ddply(econComputed, .(EVTYPE), summarize, econTotal=sum(econTotal))
+topEcon <- head(econSums[order(econSums$econTotal, na.last = TRUE, decreasing = TRUE),])
+ggplot(topEcon, aes(x = factor(EVTYPE), y = econTotal)) + geom_bar(stat = "identity")
+ggplot(topEcon, aes(x = factor(EVTYPE), y = econTotal)) + geom_bar(stat = "identity", fill = "#FF9999")
